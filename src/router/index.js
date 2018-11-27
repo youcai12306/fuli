2c14832b0eba0b4af543c3ec473ece39eea8ae97
youcai12306 <378315310@qq.com>
2018-11-27 10:55:22 +0800

    111

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from '@/view/Login'
 import McGrady from '@/view/McGrady'
 import News from '@/view/News'
@@ -10,11 +9,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/helloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/',
       name: 'Login',

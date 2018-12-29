b87de6d241832338342b98014785cdc54ddf72ca
lxy <2212884878@qq.com>
2018-12-29 18:56:37 +0800

    新闻中心

--- src/view/news/NoticeDetail.vue
+++ src/view/news/NoticeDetail.vue
@@ -109,6 +109,28 @@ export default {
     }
     .content{
         margin-top: 30px;
+				height: 950px;
+				overflow-y: auto;
+				&::-webkit-scrollbar {
+					/*滚动条整体样式*/
+					width: 10px;
+					/*高宽分别对应横竖滚动条的尺寸*/
+					height: 1px;
+				}
+				
+				&::-webkit-scrollbar-thumb {
+					/*滚动条里面小方块*/
+					border-radius: 10px;
+					background-color: #F90;
+					background-image: -webkit-linear-gradient(45deg, rgba(255, 255, 255, .2) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .2) 50%, rgba(255, 255, 255, .2) 75%, transparent 75%, transparent);
+				}
+				
+				&::-webkit-scrollbar-track {
+					/*滚动条里面轨道*/
+					-webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+					/* border-radius: 10px; */
+					background: #EDEDED;
+				}
     }
   }
 }
